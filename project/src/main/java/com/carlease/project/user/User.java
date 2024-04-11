ffd1228f258ec5304934b8da196e066e54c0a63c
Ingrida <ingridarrr@gmail.com>
2024-04-11 19:06:27 +0300

    h2 user mock data, user getAll, getById endpoints

--- project/src/main/java/com/carlease/project/user/User.java
+++ project/src/main/java/com/carlease/project/user/User.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
