cbd8722736a873886cf324f72d62641fe216d63b
nojusbuc <noahmb.nmb@gmail.com>
2024-04-15 12:11:08 +0300

    changing cors config to allow all origins for easier development

--- project/src/main/java/com/carlease/project/config/WebConfig.java
+++ project/src/main/java/com/carlease/project/config/WebConfig.java
@@ -6,17 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private String[] ORIGINS = {
-            "http://localhost:8080",
-            "https://car-leasing-project-front-sandbox.onrender.com",
-            "https://car-leasing-project-front-main.onrender.com"};
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(ORIGINS)
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
