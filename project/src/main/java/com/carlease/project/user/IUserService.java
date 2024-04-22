96785e45677b00cc078a4708babdc85994105bc7
Rychka <@ryckutis>
2024-04-22 13:58:06 +0300

    Add Login exception for User interface

--- project/src/main/java/com/carlease/project/user/IUserService.java
+++ project/src/main/java/com/carlease/project/user/IUserService.java
@@ -2,13 +2,15 @@ package com.carlease.project.user;
 
 import com.carlease.project.user.exceptions.IncorrectPasswordException;
 import com.carlease.project.user.exceptions.UserNotFoundException;
-import com.carlease.project.user.exceptions.UsernameNotFoundException;
 
 import java.util.List;
 
 public interface IUserService {
     List<User> findAll();
+
     User findById(long id) throws UserNotFoundException;
-    UserSession login(String username, String password) throws UsernameNotFoundException, IncorrectPasswordException;
+
+    UserSession login(String username, String password) throws IncorrectPasswordException;
+
     User createUser(User user);
 }
