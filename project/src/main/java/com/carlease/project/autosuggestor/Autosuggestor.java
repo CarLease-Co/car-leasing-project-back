29ccfe121ae1196905bc9fcb519253de71c2605a
ingridarev <ingridarrr@gmail.com>
2024-04-23 14:25:56 +0300

    add table name

--- project/src/main/java/com/carlease/project/autosuggestor/Autosuggestor.java
+++ project/src/main/java/com/carlease/project/autosuggestor/Autosuggestor.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "autosuggestors")
 public class Autosuggestor {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
