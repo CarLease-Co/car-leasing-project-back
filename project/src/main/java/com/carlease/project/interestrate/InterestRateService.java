0833cbe4ec5f1193e2a7b914223b1384884f955f
Rychka <@ryckutis>
2024-04-24 14:57:36 +0300

    Change "No need for annotation here"

--- project/src/main/java/com/carlease/project/interestrate/InterestRateService.java
+++ project/src/main/java/com/carlease/project/interestrate/InterestRateService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface InterestRateService {
     List<InterestRate> findAll();
     InterestRate findAndUpdate(InterestRate interestRate);
