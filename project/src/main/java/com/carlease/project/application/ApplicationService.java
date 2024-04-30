8b8c1618f5dc454a53e87b88f5b576a9d551e489
ingridarev <ingridarrr@gmail.com>
2024-04-30 14:20:28 +0300

    update delete method with user role

--- project/src/main/java/com/carlease/project/application/ApplicationService.java
+++ project/src/main/java/com/carlease/project/application/ApplicationService.java
@@ -30,6 +30,6 @@ public interface ApplicationService {
 
     AutosuggestorDto findAutosuggestorByApplicationId(long id) throws AutosuggestorNotFoundException, ApplicationNotFoundException;
 
-    boolean deleteById(long id) throws ApplicationNotFoundException;
+    boolean deleteById(long applicationId, long userId, UserRole role) throws ApplicationNotFoundException, UserException;
 
 }
